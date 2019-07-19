c00e3af613554a98cfa700bc2949b84c0b827e65
yataro <teoh6g@gmail.com>
2019-07-19 16:40:39 +0800

    Completed hero with responsive design

--- Front-End/components/WeatherHero.js
+++ Front-End/components/WeatherHero.js
@@ -1,3 +1,6 @@
+import { faCloudSun } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 export default function WeatherHero(props){
   const currentDate = new Date();
   const monthName = [
@@ -25,11 +28,31 @@ export default function WeatherHero(props){
     'Sunday'
   ];
 
+  const weatherIcons = [
+    'clear-day',
+    'clear-night',
+    'rain',
+    'snow',
+    'sleet',
+    'wind',
+    'fog',
+    'cloudy',
+    'partly-cloudy-day',
+    'partly-cloudy-night'
+  ]
+
   return (
     <div className='col-12'>
       <div className='card hero'>
-				<h1 className='location'>Kuala Lumpur</h1>
-				<p className='date'>{weekName[currentDate.getDay()]}, &nbsp;{currentDate.getUTCDate()} {monthName[currentDate.getUTCMonth()]}</p>
+        <div className='container-left'>
+          <h1 className='location'>Kuala Lumpur</h1>
+          <p className='date'>{weekName[currentDate.getDay()]}, &nbsp;{currentDate.getUTCDate()} {monthName[currentDate.getUTCMonth()]}</p>
+        </div>
+        <div className='container-right'>
+          <FontAwesomeIcon className='weather-icon' icon={faCloudSun} />
+          <p className='temperature'><b>23<sup className='temperature-format'>°</sup></b></p>
+          
+        </div>
       </div>
     </div>
   )
