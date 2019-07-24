55d1e63d7d3e9d6baffccac42ba5723eeb0cf496
yataro <teoh6g@gmail.com>
2019-07-24 22:55:32 +0800

    Geolocation and API call handler

--- Front-End/pages/index.js
+++ Front-End/pages/index.js
@@ -1,32 +1,76 @@
+import React from 'react';
+
 import Layout from '../components/Layout.js';
 import WeatherHero from '../components/WeatherHero.js';
 import WeatherInfo from '../components/WeatherInfo.js';
 import WeatherWeekly from '../components/WeatherWeekly.js';
-import Link from 'next/link';
-import Head from 'next/head';
+import fetch from 'isomorphic-unfetch';
 
 //CSS file
 import "../styles/main.scss";
 
-const PostLink = props => (
-  <li>
-    <Link href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
-);
+class Dashboard extends React.Component {
 
-export default function Blog() {
-  const currentDate = new Date();
+  constructor(props){
+    super(props);
+    this.state = {
+      currentDate: new Date(),
+      currentLat: 51.5074,
+      currentLong: 0.1278,
+      weatherSummary: '',
+      weatherCurrently: '',
+      weatherWeekly: '',
+      weatherSunrise: '',
+      weatherSunset: ''
+    };
+  }
 
-  return (
+  componentDidMount() {
+
+    var geoDataPromise = new Promise(function(resolve, reject) {
+      if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          resolve({lat: position.coords.latitude, long: position.coords.longitude})
+        }); 
+      }
+    });
+
+    geoDataPromise.then((pos) => {
+      this.setState({
+        currentLat: pos.lat.toFixed(4),
+        currentLong: pos.long.toFixed(4)
+      });
+
+      fetch(''.concat('https://api-weathery.herokuapp.com/weather?lat=', this.state.currentLat, '&long=', this.state.currentLong))
+        .then(response => response.json())
+        .then(json => {
+          this.setState({
+            weatherSummary: ''.concat(json.weatherData.currently.summary, ' | ', json.weatherData.daily.summary),
+            weatherSunrise: json.weatherData.daily.data[1].sunriseTime,
+            weatherSunset: json.weatherData.daily.data[1].sunsetTime,
+            weatherCurrently: json.weatherData.currently,
+            weatherWeekly: json.weatherData.daily.data
+          })
+          console.log(weatherSummary, weatherSunrise, weatherSunset);
+        })
+
+    });
+
+    
+    
+  }
+
+  render() {
+    const currentDate = new Date();
+
+    return (
     <Layout>
       <div className='container'>
-        <WeatherHero date={currentDate}/>
+        <WeatherHero date={this.state.currentDate}/>
         
-        <WeatherInfo date={currentDate}/>
+        <WeatherInfo date={this.state.currentDate}/>
 
-        <WeatherWeekly date={currentDate}/>
+        <WeatherWeekly date={this.state.currentDate}/>
         
         <div className='col-12'>
           <div className='card'>
@@ -34,5 +78,9 @@ export default function Blog() {
         </div>
       </div>
     </Layout>
-  );
+    );
+  };
 }
+
+
+export default Dashboard;
