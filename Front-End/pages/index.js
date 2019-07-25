190aa8c7b38bab947cdffe95d5062d7403483c75
yataro <teoh6g@gmail.com>
2019-07-25 21:43:11 +0800

    Added API data fetching with loader

--- Front-End/pages/index.js
+++ Front-End/pages/index.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout.js';
 import WeatherHero from '../components/WeatherHero.js';
 import WeatherInfo from '../components/WeatherInfo.js';
 import WeatherWeekly from '../components/WeatherWeekly.js';
-import fetch from 'isomorphic-unfetch';
+import axios from 'axios';
 
 //CSS file
 import "../styles/main.scss";
@@ -14,6 +14,7 @@ class Dashboard extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      loading: true,
       currentDate: new Date(),
       currentLat: 51.5074,
       currentLong: 0.1278,
@@ -21,7 +22,8 @@ class Dashboard extends React.Component {
       weatherCurrently: '',
       weatherWeekly: '',
       weatherSunrise: '',
-      weatherSunset: ''
+      weatherSunset: '',
+      weatherTimezone: ''
     };
   }
 
@@ -40,44 +42,74 @@ class Dashboard extends React.Component {
         currentLat: pos.lat.toFixed(4),
         currentLong: pos.long.toFixed(4)
       });
-
-      fetch(''.concat('https://api-weathery.herokuapp.com/weather?lat=', this.state.currentLat, '&long=', this.state.currentLong))
-        .then(response => response.json())
-        .then(json => {
-          this.setState({
-            weatherSummary: ''.concat(json.weatherData.currently.summary, ' | ', json.weatherData.daily.summary),
-            weatherSunrise: json.weatherData.daily.data[1].sunriseTime,
-            weatherSunset: json.weatherData.daily.data[1].sunsetTime,
-            weatherCurrently: json.weatherData.currently,
-            weatherWeekly: json.weatherData.daily.data
-          })
-          console.log(weatherSummary, weatherSunrise, weatherSunset);
+        
+        let weatherUrl = ''.concat('https://api-weathery.herokuapp.com/weather?lat=', this.state.currentLat, '&long=', this.state.currentLong);
+        axios({
+          method: 'get',
+          url: weatherUrl,
+          responseType: 'json',
+          data: {}
         })
-
+        .then(res => {
+          console.log(res.data);
+          this.setState({
+            loading: false,
+            weatherSummary: ''.concat(res.data.weatherData.currently.summary, ' | ', res.data.weatherData.daily.summary),
+            weatherSunrise: res.data.weatherData.daily.data[1].sunriseTime*1000,
+            weatherSunset: res.data.weatherData.daily.data[1].sunsetTime*1000,
+            weatherCurrently: res.data.weatherData.currently,
+            weatherWeekly: res.data.weatherData.daily.data,
+            weatherTimezone: res.data.weatherData.timezone
+          })
+          console.log(this.state.weatherSummary, this.state.weatherSunrise, this.state.weatherSunset);
+        })
     });
 
-    
-    
   }
 
   render() {
-    const currentDate = new Date();
+    const {
+      loading,
+      currentDate,
+      weatherSummary,
+      weatherSunrise,
+      weatherSunset,
+      weatherCurrently,
+      weatherWeekly,
+      weatherTimezone
+    } = this.state;
 
-    return (
-    <Layout>
-      <div className='container'>
-        <WeatherHero date={this.state.currentDate}/>
-        
-        <WeatherInfo date={this.state.currentDate}/>
+    return (loading) ? (
+      <Layout>
+        <div className='loader'></div>
+      </Layout>
+    ) : (
+      <Layout>
+        <div className='container'>
+          <WeatherHero 
+            date={currentDate} 
+            currently={weatherCurrently}
+            timezone={weatherTimezone}
+          />
+          
+          <WeatherInfo 
+            currently={weatherCurrently}
+            summary={weatherSummary}
+          />
 
-        <WeatherWeekly date={this.state.currentDate}/>
-        
-        <div className='col-12'>
-          <div className='card'>
+          <WeatherWeekly
+            date={currentDate}
+            sunrise={weatherSunrise}
+            sunset={weatherSunset}
+            weekly={weatherWeekly} 
+          />
+          
+          <div className='col-12'>
+            <div className='card'>
+            </div>
           </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
     );
   };
 }
