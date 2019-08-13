b5c9aa4e00db01a43baa3274560eaa0a4487cbda
yataro <teoh6g@gmail.com>
2019-08-13 15:01:18 +0800

    Modified API to support language settings

--- Back-End/handlers/weather.js
+++ Back-End/handlers/weather.js
@@ -9,8 +9,9 @@ exports.getWeather = (req, res) => {
     const queryLong = req.query.long || 0;
     const queryExclude = req.query.exclude || 'minutely,hourly,alerts,flags';
     const queryUnits = (['us', 'si'].indexOf(req.query.units) === -1)? 'si' : req.query.units;
+    const queryLang = req.query.lang || 'en';
 
-    const reqUrl =  `https://api.darksky.net/forecast/${process.env.API_KEY}/${queryLat},${queryLong}?exclude=${queryExclude}&units=${queryUnits}`
+    const reqUrl =  `https://api.darksky.net/forecast/${process.env.API_KEY}/${queryLat},${queryLong}?exclude=${queryExclude}&units=${queryUnits}&lang=${queryLang}`
     
     request(reqUrl, function (error, api_res, body) {
         const statusCode = api_res.statusCode;
